01e7f4b511362e43e8f9521138a0ec5d5b9b6039
Teanka <arolicka@gmail.com>
2018-11-19 12:39:36 +0100

    first part

---
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -0,0 +1,14 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/test")
+    public String hello(){
+        System.out.println("!!!!");
+        return "hello";
+    }
+}
